4d224db76fb0a80d9629fe672470da7573f6d02b
Minhaz Ul Mostofa Pranto <zminhazul29@gmail.com>
2024-12-16 22:13:57 +0600

    Add files via upload

---
+++ Codeforces/705_A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+     int n;
+     cin>>n;
+
+    
+
+
+     for(int i=1;i<=n;i++)
+     {
+        if(i>1)
+        {
+            cout<<" that ";
+        }
+        if(i%2==0)
+        {
+            cout<<"I love";
+        }
+        else if(i%2!=0)
+        {
+            cout<<"I hate";
+        }
+        
+     }
+
+     cout<<" it";
+
+
+    return 0;
+}
